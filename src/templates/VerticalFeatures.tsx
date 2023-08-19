d51add9ed543b6921fb2262cadce93c043587498
Michal Půlpán <me@michalpulpan.eu>
2023-08-19 08:52:55 +0200

    imgs on landing page

--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -11,20 +11,20 @@ const VerticalFeatures = () => (
       <VerticalFeatureRow
         title="User oriented dashboard"
         description="Our dashboard is a symphony of innovation, delivering real-time insights and intuitive control."
-        image="/assets/images/catalog_product_edit_media_reorder.gif"
+        image="/assets/images/overview.png"
         imageAlt="First feature alt text"
       />
       <VerticalFeatureRow
         title="AI based recommendation system"
         description="We go beyond, embracing the art of recommendation to engage users and boosting sales like. ecoseller’s recommendation system doesn’t just sell, it captivates, creating personalized experiences that fuel engagement."
-        image="" //"/assets/images/feature2.svg"
+        image="/assets/images/recommendation.svg"
         imageAlt="Second feature alt text"
         reverse
       />
       <VerticalFeatureRow
         title="Extensible and customizable"
         description="ecoseller aims to extensible platofrm, powered by our notification API. Seamlessly connect events, unlock the power to call external APIs – it’s your gateway to endless possibilities, making ecoseller not just a platform but a universe of opportunities."
-        image="" //"/assets/images/feature3.svg"
+        image="/assets/images/extendibility.svg"
         imageAlt="Third feature alt text"
       />
     </Section>
